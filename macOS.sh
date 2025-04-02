ecd5560bfa715e70560e9318f8058a82ceab1a5b
Axel Quack <mail@axelquack.de>
2025-04-02 18:56:28 +0200

    Update macOS.sh: Updated to macOS Sequoia

--- macOS.sh
+++ macOS.sh
@@ -3,25 +3,25 @@
 # Ask for the administrator password upfront
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# ###################################################################################################################
-# GENERAL UI/UX
-# ###################################################################################################################
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
-# Enable snap-to-grid for desktop icons
+# Enable snap-to-grid for icons on the desktop and in other icon views
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Disable opening and closing window animations
+# Disable window animations
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
-
-
-# ###################################################################################################################
-# TRACKPAD, MOUSE, KEYBOARD, BLUETOOTH, ACCESSORIES AND INPUT
-# ###################################################################################################################
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
+###############################################################################
 
 # Automatically illuminate built-in MacBook keyboard in low light
 defaults write com.apple.BezelServices kDim -bool true
@@ -29,58 +29,38 @@ defaults write com.apple.BezelServices kDim -bool true
 # Turn off keyboard illumination when computer is not used for 5 minutes
 defaults write com.apple.BezelServices kDimTime -int 300
 
+###############################################################################
+# Finder                                                                      #
+###############################################################################
 
-
-# ###################################################################################################################
-# FINDER
-# ###################################################################################################################
-
-# Finder: show all filename extensions
+# Show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Show hidden files in Finder
+# Show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
-# Show icons for hard drives, servers, and removable media on the desktop
+# Show icons for external hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
+###############################################################################
+# Menu Bar                                                                    #
+###############################################################################
 
-
-# ###################################################################################################################
-# MENU BAR
-# ###################################################################################################################
-
-# Show the battery percentage in the menu bar
+# Show battery percentage
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
-
-
-# ###################################################################################################################
-# DOCK & HOT CORNER
-# ###################################################################################################################
-
-# Disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
-
-# Don’t show Dashboard as a Space
-defaults write com.apple.dock dashboard-in-overlay -bool true
+###############################################################################
+# Dock & Hot Corners                                                          #
+###############################################################################
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
-
-# Hot corners
-# Bottom left screen corner → Start screen saver
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
-
-# Disable mission control and spaces, and rejoice
-defaults write com.apple.dock mcx-expose-disabled -bool TRUE
 
 # Hot corners
 # Possible values:
@@ -90,25 +70,25 @@ defaults write com.apple.dock mcx-expose-disabled -bool TRUE
 #  4: Desktop
 #  5: Start screen saver
 #  6: Disable screen saver
-#  7: Dashboard
 # 10: Put display to sleep
 # 11: Launchpad
 # 12: Notification Center
+
 # Top left screen corner → Mission Control
 defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-tl-modifier -int 0
+
 # Top right screen corner → Desktop
 defaults write com.apple.dock wvous-tr-corner -int 4
 defaults write com.apple.dock wvous-tr-modifier -int 0
+
 # Bottom left screen corner → Start screen saver
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
-
-
-# ###################################################################################################################
-# SAFARI & WEBKIT
-# ##################################################################################################################
+###############################################################################
+# Safari & WebKit                                                             #
+###############################################################################
 
 # Set Safari’s home page to `about:blank` for faster loading
 defaults write com.apple.Safari HomePage -string "about:blank"
@@ -124,13 +104,11 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
+###############################################################################
+# Mail                                                                        #
+###############################################################################
 
-
-# ###################################################################################################################
-# MAIL
-# ###################################################################################################################
-
-# Display emails in threaded mode, sorted by date (oldest at the top)
+# Display emails in threaded mode, sorted by date, oldest at the top
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
@@ -138,36 +116,41 @@ defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -stri
 # Disable inline attachments (just show the icons)
 defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
 
-
-
-# ###################################################################################################################
-# TIME MACHINE
-# ###################################################################################################################
+###############################################################################
+# Time Machine                                                                #
+###############################################################################
 
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# Exclude certain system files from the backup
-tmutil setexclusion -p /Volumes/TimeMachine /System /Applications /Library /usr
+# Exclude system files from backups (Note: Time Machine may already exclude these)
+# tmutil addexclusion -p /System
+# tmutil addexclusion -p /Applications
+# tmutil addexclusion -p /Library
+# tmutil addexclusion -p /usr
 
-# Set the backup interval to 6 hours (21600 seconds)
-defaults write /System/Library/LaunchDaemons/com.apple.backupd-auto
+# Exclude Desktop from backups
+# tmutil addexclusion -p ~/Desktop
 
-# Exclude the "Desktop" folder from the backup
-tmutil addexclusion -p /Users/yourusername/Desktop
+# Disable local Time Machine snapshots (Note: This may not be necessary in recent macOS versions)
+sudo tmutil disablelocal
 
-# Disable local snapshots
-tmutil disablelocal
-
-
-
-# ###################################################################################################################
-# TEXT EDIT
-# ###################################################################################################################
+###############################################################################
+# TextEdit                                                                    #
+###############################################################################
 
 # Use plain text mode for new TextEdit documents
 defaults write com.apple.TextEdit RichText -int 0
 
-# Open and save files as UTF-8 in TextEdit
+# Set encoding to UTF-8
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+for app in "Finder" "Dock" "Safari" "Mail" "SystemUIServer"; do
+    killall "${app}" > /dev/null 2>&1
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
